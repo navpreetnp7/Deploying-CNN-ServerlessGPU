1af5648d7ef4747172a9037d8adf3bf108d7c4b1
Navpreet Singh <navpreet_np@yahoo.com>
2023-03-15 01:21:02 -0400

    modifying test

--- Dockerfile
+++ Dockerfile
@@ -16,8 +16,8 @@ ADD server.py .
 
 # Add your model weight files 
 # (in this case we have a python script)
-ADD download.py .
-RUN python3 download.py
+ADD convert_to_onnx.py .
+RUN python3 convert_to_onnx.py
 
 
 # Add your custom app code, init() and inference()
--- test_onnx.py
+++ test_onnx.py
@@ -1,22 +1,22 @@
 import torch
 from PIL import Image
-from model import OnnxModel
+from model.model import OnnxModel
+
+global model
+path = "model/classifier.onnx"
+model = OnnxModel(path)
 
 class TestOnnx:
 
     def test_tench(self):
-        path = "model/classifier.onnx"
-        model = OnnxModel(path)
-
+        global model
         img = Image.open("images/n01440764_tench.JPEG")
         inp = model.preprocess_numpy(img).unsqueeze(0) 
         res = model.predict(inp)
         assert torch.argmax(torch.Tensor(res)) == torch.Tensor([0])
 
     def test_turtle(self):
-        path = "model/classifier.onnx"
-        model = OnnxModel(path)
-
+        global model
         img = Image.open("images/n01667114_mud_turtle.JPEG")
         inp = model.preprocess_numpy(img).unsqueeze(0) 
         res = model.predict(inp)
