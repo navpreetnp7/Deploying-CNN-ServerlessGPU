57d8b9d62720d8d3732f711c370e8cbfe3f5661a
Navpreet Singh <navpreet_np@yahoo.com>
2023-03-15 03:42:52 -0400

    Testing deployment

--- test_onnx.py
+++ test_onnx.py
@@ -1,6 +1,8 @@
 import torch
 from PIL import Image
 from model.model import OnnxModel
+from io import BytesIO
+import requests
 
 global model
 path = "model/classifier.onnx"
@@ -10,12 +12,16 @@ class TestOnnx:
 
     def test_tench(self):
         global model
-        img = Image.open("images/n01440764_tench.JPEG")
+        img_path = "https://raw.githubusercontent.com/MTailorEng/mtailor_mlops_assessment/main/n01440764_tench.jpeg"
+        response = requests.get(img_path)
+        img = Image.open(BytesIO(response.content))
         res = model.predict(img)
         assert torch.argmax(torch.Tensor(res)) == torch.Tensor([0])
 
     def test_turtle(self):
         global model
-        img = Image.open("images/n01667114_mud_turtle.JPEG")
+        img_path = "https://raw.githubusercontent.com/MTailorEng/mtailor_mlops_assessment/main/n01667114_mud_turtle.JPEG"
+        response = requests.get(img_path)
+        img = Image.open(BytesIO(response.content))
         res = model.predict(img)
         assert torch.argmax(torch.Tensor(res)) == torch.Tensor([35])
--- test_server.py
+++ test_server.py
@@ -6,7 +6,7 @@ import banana_dev as banana
 api_key = "79b191e0-8392-4e6b-bc52-f1440148e59a"
 model_key = "2166cbfc-ce47-45e2-aa6f-bf87d6059230"
 
-img_path = "https://github.com/MTailorEng/mtailor_mlops_assessment/blob/main/n01440764_tench.jpeg"
+img_path = "https://raw.githubusercontent.com/MTailorEng/mtailor_mlops_assessment/main/n01440764_tench.jpeg"
 model_inputs = {'input': img_path}
 
 response = banana.run(api_key, model_key, model_inputs)
