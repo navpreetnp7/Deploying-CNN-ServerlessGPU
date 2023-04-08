7927c6a7669b984e64caa2824e5eab0577cd522b
Navpreet Singh <navpreet_np@yahoo.com>
2023-04-08 16:26:57 -0400

    Update pytorch_model.py

--- model/pytorch_model.py
+++ model/pytorch_model.py
@@ -311,12 +311,12 @@ class Classifier(nn.Module):
 
 
 if __name__ == "__main__":
-    mtailor = Classifier(BasicBlock, [2, 2, 2, 2])
-    mtailor.load_state_dict(torch.load("./resnet18-f37072fd.pth"))
-    mtailor.eval()
+    model = Classifier(BasicBlock, [2, 2, 2, 2])
+    model.load_state_dict(torch.load("./resnet18-f37072fd.pth"))
+    model.eval()
     
     img = Image.open("./n01667114_mud_turtle.JPEG")
-    inp = mtailor.preprocess_numpy(img).unsqueeze(0) 
-    res = mtailor.forward(inp)
+    inp = model.preprocess_numpy(img).unsqueeze(0) 
+    res = model.forward(inp)
 
     print(torch.argmax(res))
